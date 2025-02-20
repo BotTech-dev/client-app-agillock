13fc279ed080ebeb36143882103a55cabd7af878
Fernando Barros <fernando.barros.oliveira@gmail.com>
2025-02-19 22:32:36 -0300

    construçao parcial da tela inicial

--- lib/pages/incial_page.dart
+++ lib/pages/incial_page.dart
@@ -14,11 +14,31 @@ class _InicialPageState extends State<InicialPage> {
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text('AgilLock'),
+          backgroundColor: Colors.black,
+          toolbarHeight: 210,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFFFEB22D), width: 4), // Borda amarela
+                  borderRadius: BorderRadius.circular(8), // Bordas arredondadas
+                ),
+                child: Image.asset(
+                  'assets/images/logo.png', // Caminho da imagem na pasta assets
+                  height: 200, // Ajustando o tamanho da imagem
+                ),
+              ),
+              const SizedBox(width: 10),
+              //const Text("Registrar Bebida"),
+            ],
+          ),
+          centerTitle: true,
         ),
         body: Center(
             child: Container(
-              width: 400,
+              width: 500,
               color: Colors.black,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -26,17 +46,20 @@ class _InicialPageState extends State<InicialPage> {
                 children: [
 
                   const SizedBox(height: 32,),
-                  ElevatedButton(
-                    onPressed: (){
-
-
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
+                  SizedBox(
+                    width: double.infinity, // Faz o botão ocupar toda a largura
+                    child: ElevatedButton(
+                      onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16), // Aumenta a altura
+                        textStyle: const TextStyle(fontSize: 18), // Aumenta o tamanho do texto
+                        backgroundColor: const Color(0xFFFEB22D),
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Text("Salvar"),
                     ),
-                    child: const Text('Entrar'),
                   ),
+
                 ],
               ),
 
